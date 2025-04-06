7d22924899584a681d449a152199e6ac59a680c7
Denisse Alcantara <dbalcantara@up.edu.ph>
2025-04-06 20:43:11 +0800

    fix: gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import ProjectPage from './components/ProjectPage';
 
 
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <>
+    <button onClick={() => navigate('/')}>Home</button>
+    <button onClick={() => navigate('/about')}>About</button>
+    <button onClick={() => navigate('/project')}>About</button>
+
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
