de1daa8d108c2a291bbb2c0953bd75cbea7f1b1d
Denisse Alcantara <dbalcantara@up.edu.ph>
2025-04-06 17:38:42 +0800

    fix: for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter} from 'react-router-dom';
+import './index.css';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <HashRouter basename={process.env.PUBLIC_URL}>
-  <StrictMode>
+   <React.StrictMode>
     <App />
-  </StrictMode>
+   </React.StrictMode>
   </HashRouter>
-
-)
+);
