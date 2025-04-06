73ce9130539e0da71e81ca9f389cd3f8f2036437
Denisse Alcantara <dbalcantara@up.edu.ph>
2025-04-06 20:35:38 +0800

    fix: gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import './App.css'
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import ProjectPage from './components/ProjectPage';
-import { HashRouter} from 'react-router-dom';
 
 
 
 function App() {
   return (
     <>
-      <Router basename="/denisse-alcantara">
         <Routes>
-        <Route path="/" element={ HomePage() } />
-        <Route path="/about" element={ AboutPage() } />
-        <Route path="/projects" element={ ProjectPage() } />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/project" element={<ProjectPage />} />
         </Routes>
-      
-    </Router>
-
 
     </>
 
