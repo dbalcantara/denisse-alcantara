1ada7bac9341d95545f502cbcf1dfe779028b14f
Denisse Alcantara <dbalcantara@up.edu.ph>
2025-04-12 11:45:51 +0800

    fix: photos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <p className='name'>DENISSE ALCANTARA</p>
         <div className='made-by' >
         <p className='foot-det'>handcrafted by</p>
-          <img  className='heart' src='heart.gif'/>
+          <img  className='heart' src='images/heart.gif'/>
           <p className='foot-det'>yours truly</p>
         </div>
         <p className='foot-det'>Taguig City</p>
