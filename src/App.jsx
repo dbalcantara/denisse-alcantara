08a86e43638f8135364fa553e5b25a2445717337
Denisse Alcantara <dbalcantara@up.edu.ph>
2025-04-12 11:05:12 +0800

    fix: photos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import ProjectPage from './components/ProjectPage';
 import { Routes, Route, HashRouter } from "react-router-dom";
-import './assets/*';
 
 function App() {
   return (
